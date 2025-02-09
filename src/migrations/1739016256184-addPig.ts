2f7c2c431511b3f4f0cb770ba68fc9400b8a2eff
luukitto <luukitto@gmail.com>
2025-02-09 15:14:59 +0400

    Update: set main pig to Bidzina

--- src/migrations/1739016256184-addPig.ts
+++ src/migrations/1739016256184-addPig.ts
@@ -4,14 +4,14 @@ export class AddPig1739016256184 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
             INSERT INTO pig (name, status) 
-            VALUES ('Animal Farm Pig', 'default')
+            VALUES ('Bidzina', 'default')
             ON CONFLICT DO NOTHING
         `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            DELETE FROM pig WHERE name = 'Animal Farm Pig'
+            DELETE FROM pig WHERE id = 1;
         `);
     }
 }
